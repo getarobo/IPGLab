d677e5405732854f854cbd4d86d8073e097f01cc
getarobo <thefightingbee@gmail.com>
2015-03-18 19:00:13 -0400

    Initial Commit

---
+++ TwoView/AppDelegate.m
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.m
+//  TwoView
+//
+//  Created by Gene Han on 3/18/15.
+//  Copyright (c) 2015 Algorithm. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@end
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+    // Insert code here to initialize your application
+}
+
+- (void)applicationWillTerminate:(NSNotification *)aNotification {
+    // Insert code here to tear down your application
+}
+
+@end
---
+++ TwoView/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  TwoView
+//
+//  Created by Gene Han on 3/18/15.
+//  Copyright (c) 2015 Algorithm. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
